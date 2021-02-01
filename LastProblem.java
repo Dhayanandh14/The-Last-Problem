8286e2e6f67976944331e2e4fc98428dd951022b
Dhayanandh <69616308+Dhayanandh14@users.noreply.github.com>
2021-02-01 10:57:43 +0530

    Add files via upload

---
+++ LastProblem.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class LastProblem{
+  public static void main(String[] args) {
+    Scanner kbd = new Scanner(System.in);
+    String s = kbd.nextLine();
+    System.out.print("Thank you, "+s+", and farewell!");
+  }
+}
